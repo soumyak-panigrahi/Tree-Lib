2df113ff46e304a32e130ea6e761340d02a0593d
Soumyak Panigrahi <soumyakpanigrahi123@gmail.com>
2021-07-11 20:03:05 +0530

    Start of Binary Search Tree

--- include/tree/bsTree.h
+++ include/tree/bsTree.h
@@ -1,38 +1,55 @@
 #ifndef __BS_TREE_H__
 #define __BS_TREE_H__
 
+#include <binaryTree.h>
 #include <memory>
 
-template<typename T> struct BStrNode;
-template<typename T> class bsTree;
+template<typename T, typename __type> struct BStrNode;
+template<typename T, typename __type> class bsTree;
 
-template<typename T>
-using wPtrBStr = std::weak_ptr<BStrNode<T>>;
-template<typename T>
-using sPtrBStr = std::shared_ptr<BStrNode<T>>;
-template<typename T>
-using c_wPtrBStr = const std::weak_ptr<BStrNode<T>>;
-template<typename T>
-using cwPtrBStr = std::weak_ptr<const BStrNode<T>>;
-template<typename T>
-using c_sPtrBStr = const std::shared_ptr<BStrNode<T>>;
-template<typename T>
-using csPtrBStr = std::shared_ptr<const BStrNode<T>>;
+template<typename T, typename __type>
+using wPtrBStrNode = std::weak_ptr<BStrNode<T, __type>>;
+template<typename T, typename __type>
+using sPtrBStrNode = std::shared_ptr<BStrNode<T, __type>>;
+template<typename T, typename __type>
+using c_wPtrBStrNode = const std::weak_ptr<BStrNode<T, __type>>;
+template<typename T, typename __type>
+using cwPtrBStrNode = std::weak_ptr<const BStrNode<T, __type>>;
+template<typename T, typename __type>
+using c_sPtrBStrNode = const std::shared_ptr<BStrNode<T, __type>>;
+template<typename T, typename __type>
+using csPtrBStrNode = std::shared_ptr<const BStrNode<T, __type>>;
 
-template<typename T>
-struct BStrNoe
+template<typename T, typename __type>
+struct BStrNode : public I_BINARYtrNode<__type>
 {
     T data;
-    wPtrBStr<T> p;
-    sPtrBStr<T> left, right;
+    wPtrBStrNode<T, __type> p;
+    sPtrBStrNode<T, __type> left, right;
     BStrNode(T x = T{}) : data{ x }, left{ nullptr }, right{ nullptr } { }
+    virtual const __type& read() const noexcept
+    {
+        __type temp{ data };
+        return temp;
+    }
+
+    virtual bool write(const __type& x)
+    {
+        if (x = data)
+            return true;
+        return false;
+    }
+
+    virtual ~BStrNode() = default;
 };
 
-template<typename T>
-class bsTree : public Tree
+template<typename T, typename __type>
+class bsTree : public I_BINARYtr
 {
+    sPtrBStrNode<T, __type> root;
+    size_t size;
 public:
+
 };
 
-
-#endif //__BS_TREE_H__
+#endif // __BS_TREE_H__
